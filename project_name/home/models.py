1fd47d3a778e157b3ee186a7e6b7e3163dba7e42
ilfan <ilfanmuratov@gmail.com>
2021-08-07 23:47:19 +0500

    feat: logo template added

--- project_name/home/models.py
+++ project_name/home/models.py
@@ -4,4 +4,13 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    """Home page model."""
+
+    max_count = 1
+    is_main = True
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Титульная страница'
